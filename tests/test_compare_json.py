0f529cb946df6dcca1498c8c9fc16b1fa5c090c1
marcin <marcin.konowalczyk@canonical.com>
2025-08-18 21:20:16 +0100

    more tests

--- tests/test_compare_json.py
+++ tests/test_compare_json.py
@@ -1,4 +1,5 @@
 from compare_json import compare_json
+from compare_json.compare_json import _compare_json
 
 
 def test_compare_json_functionality() -> None:
@@ -8,4 +9,72 @@ def test_compare_json_functionality() -> None:
 
     ok, message = compare_json(json1, json2)
 
-    assert ok, f"Comparison failed: {message}"
+    assert ok
+    assert message == ""
+
+
+def test_compare_json_different_types() -> None:
+    # Test comparing different types
+    json1 = {"key": "value"}
+    json2 = ["value"]
+
+    ok, message = compare_json(json1, json2, raise_assertion=False)
+
+    assert not ok
+    assert "Expected a dictionary" in message
+
+
+def test_compare_json_unordered_lists() -> None:
+    # Test comparing unordered lists
+    json1 = {"key": [1, 2, 3]}
+    json2 = {"key": [3, 2, 1]}
+
+    ok, _message = compare_json(json1, json2, raise_assertion=False, unordered=False)
+    assert not ok
+    ok, _message = compare_json(json1, json2, raise_assertion=False, unordered=True)
+    assert ok
+
+
+def test_compare_json_nested_structures() -> None:
+    # Test comparing nested structures
+    json1 = {"a": {"b": {"c": "d"}}}
+    json2 = {"a": {"b": {"c": "d"}}}
+
+    ok, message = compare_json(json1, json2)
+    assert ok, f"Comparison failed for nested structures: {message}"
+
+    json3 = {"a": {"b": {"c": "not d"}}}
+
+    ok, message = compare_json(json1, json3, raise_assertion=False)
+    assert not ok
+    assert "Value mismatch" in message
+
+
+def test_json_stack() -> None:
+    # Test that a stack trace is provided on failure
+    json1 = {"key": "value"}
+    json2 = {"key": "different_value"}
+
+    msg, stack = _compare_json(json1, json2, False, None)
+    assert "Value mismatch" in msg
+    assert stack == ["key"]
+
+
+def test_json_stack_deep() -> None:
+    # Test that a stack trace is provided for deep nested structures
+    json1 = {"a": {"b": {"c": "value"}}}
+    json2 = {"a": {"b": {"c": "different_value"}}}
+
+    msg, stack = _compare_json(json1, json2, False, None)
+    assert "Value mismatch" in msg
+    assert stack == ["a", "b", "c"]
+
+
+def test_json_stack_list() -> None:
+    # Test that a stack trace is provided for lists
+    json1 = {"key": ["value1", "value2"]}
+    json2 = {"key": ["value1", "different_value"]}
+
+    msg, stack = _compare_json(json1, json2, False, None)
+    assert "Value mismatch" in msg
+    assert stack == ["key", "[1]"]
